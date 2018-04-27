6c88f251a5858dd3af658e0b3eefca10869a3384
Filippo Ghibellini <ghibellini@cngroup.dk>
2018-04-27 17:15:12 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+const concatAll = xxs => [].concat(...xxs);
+
+const zIndexSelectors = (document) => {
+  const styleSheets = Array.from(document.styleSheets);
+  const zIndexSelectors = concatAll(styleSheets.map(styleSheet => {
+    const rules = Array.from(styleSheet.rules);
+    return rules
+        .filter(rule => {
+          if (rule.type !== CSSRule.STYLE_RULE) return false; // TODO rules are a tree; traverse it!
+          return Array.from(rule.style).some(s => s === 'z-index');
+        })
+        .map(rule => ({ selector: rule.selectorText, zIndex: rule.style.zIndex }));
+  }));
+  return zIndexSelectors;
+}
+
+const addCSSRule = (rule, document) => {
+  const element = document.createElement('style');
+  document.head.appendChild(element);
+  const styleSheet = Array.from(document.styleSheets).find(ss => ss.ownerNode === element);
+  styleSheet.insertRule(rule)
+}
+
+const highlightZIndexCtxs = document => {
+  const selectors = zIndexSelectors(document);
+
+  const normalSelectors = selectors.map(s => s.selector).join(', ');
+  addCSSRule(`${normalSelectors} { border: 2px dashed red !important; }`, document);
+
+  const beforeSelectors = selectors.filter(s => !s.selector.includes(':')).map(s => ({ selector: `${s.selector}::before`, zIndex: s.zIndex }));
+  beforeSelectors.forEach(s => addCSSRule(`${s.selector} { content: '${s.zIndex}'; color: red; font-weight: bold; position: absolute; left: 0.3em; top: 0.1em; }`, document));
+}
+
+highlightZIndexCtxs(document);
